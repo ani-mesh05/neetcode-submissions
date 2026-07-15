ce81437c5bde60eefc8736e7f5a0fce3d6fa7319
ani-mesh05 <166497381+ani-mesh05@users.noreply.github.com>
2026-07-15 16:14:56 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+    if(height.empty() ) return 0;
+
+    int left = 0, right = height.size() - 1;
+
+    int leftMax = height[left], rightMax = height[right];
+
+    int res = 0;
+
+    while(left < right){
+        if(leftMax < rightMax){
+            left ++;
+
+            leftMax = max(leftMax, height[left]);
+            res += leftMax - height[left];
+        }
+        else{
+            right --;
+            rightMax = max(rightMax, height[right]);
+            res += rightMax - height[right];
+        }
+    }
+    return res;
+    }
+};
